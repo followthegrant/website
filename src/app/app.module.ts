4e45ffd3ba0ae208172ba46ab494e02feecd301e
Karl Scholze <>
2018-09-30 13:30:26 +0200

    components added, search.service added, router added, navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
+import { ResultComponent } from './result/result.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    SearchResultsComponent,
+    ResultComponent,
+    NotFoundComponent,
+    HomeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'searchResults',
+        component: SearchResultsComponent
+      },
+      {
+        path: 'search',
+        component: SearchComponent
+      },
+      {
+        path: 'result/:name',
+        component: ResultComponent
+      },
+      {
+        path: '**',
+        component: NotFoundComponent
+      }])
   ],
   providers: [],
   bootstrap: [AppComponent]
